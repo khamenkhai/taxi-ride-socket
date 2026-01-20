e0f27080918b37d0484e23686670eb07863079ef
khamenkhai <khamenkhai2003@gmail.com>
2026-01-20 22:39:33 +0630

    update: refactor codes

--- src/secrets.ts
+++ src/secrets.ts
@@ -1,20 +1,5 @@
-export const serviceAccount: any = {
-  type: "service_account",
-  project_id: "project2-f5cb2",
-  private_key_id: "40db8b59f8965f04c080f0b83c5579ff195b6432",
-  private_key:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCbyM3hTNpUpov4\nP1V3zZuIqNGdgFxGXBKjArSecdrEo0hCxcxToOST7ZKT7QRKnuKigzGzD8ko26Zg\ni8eHIqwCJLI/3/YfrxRR7n6MkO+TUAAAvUvxN/xTEO9ZxsRpbrsD2MVA+p/nXLVN\nsdAZsH8PNoGY9PhdbEA0M2NEyB1DOvo6a04fgWmOr1CmEg9NDM1fUii5T4ilwJV0\nSGEane0dJXA0T7eT0dijGkFhd0TDg0szGOW1+bVOdv0AdKavZ1DJIMbbbZVfZRIi\n0MtO7w0HCmwCMms3Bg71nQK/7jyoP/AfNhf4Gmu4gqSGqR0SxrP/e8Y4pKMVaE4z\n7wYXYHynAgMBAAECggEAAcu9T9kWdT5cQ7bbMd/daKRlkja+GZOLJXTtBxyR5I1r\nUlW8ZsRmjiwBchIb6A5P14bCpmlKrN1nXBqQaMbmC7pHkcPvrurSRaWFsPIkyOJ1\ntYx5GSVHk9+QmszXS83aew9fKsIY4M5pSqV/nCQ0OsUqlkxQpqcOUGu9O7hA2hsJ\n124glOIotBOHF68G48r31F7+uqEzMIjsvAFB5vD8ZhRY6+pbsy/p09vE6cUWzK05\nLfyUOPkaDWHN/kQyMCKLalX0rLFzpcSYXHw1jgqNm5z0tKNuUMdhaDTsy2NlesVy\nPEZiQGiqVmt+TaONnyZCPfGz3Ra5DkeoOGqXf24pDQKBgQDMLL+ncgId7MNxXddy\nPXiJrc8l0yg0g+YjdALoKiWmT+3TcJXbYedZRJj7itfKpqzdALWH1X0Ug6BCbUCc\nK0rs3NAHLstvRwO2aQrAoyrXrtF6tzJZzRcEd/D8eHz1aA8mpTUKp3ITZs0D8bfV\nNfXL1VhuioO422m7j+4Qmu0uGwKBgQDDU6c9+E0dzujvTkmq2GUffbo1rx1k018D\nZBCkwLrqGrrqcPPDnPQmVE6F3hOtsm5z4tculwQorRGPr8WXiMalBcXRPnb/5nzC\n4LeghsucznV9CTennzxnpQkJuxg0uggfeBmcCl/q15ylc0aVDSR4Rp6/m6HMPM1w\nj7yKZwykZQKBgGgG2LNIzDlQ+5dUN8Q1+6cyTlu4RUDUfzpLDnwZlUsyaDsVntTD\nAiuiXsuyxxWybFrB1LvbkzoTgmC153hqOmeKbddrCS2uIf8bb+YMfHSd1o9OrbHB\nY9vb/IW2IfyrQyTugaLnA6FM/GHpEz/nmU98aO0RV+GksS9mOuZs+TFHAoGAW3Zw\nebQraCnbKTOfi0CJGZXl+/1j5jKT6yKQKJWW8MhTfjQl8RMtwET5//VdgZhr4Bf4\naviMGf/wmTfVbn+9hMiPOMvnLSzgfuB7G9ACyAplOYd3aCZIQsAk20mgrA/wfcvf\nMhIpl+4ei9rO0jy1NxzaeR6HRPuzt2GXB7f5U4ECgYA84PFLUJsC/htXxIwvus7C\nusg9nzHjr98eBpi5HYQsOQ4xm+9ZIV2jJPDYJH0vSMyLZw1sflRpqEIpmF5F7erF\nrwZugbcLa9dbJymRR9OFY/U6GX90atb+N3xKNzEEAMSOxX4BPYHQd1aqAvRhu+3e\nfZyWbf2nqTA1zzfvrRMo4A==\n-----END PRIVATE KEY-----\n",
-  client_email:
-    "project2-service-account@project2-f5cb2.iam.gserviceaccount.com",
-  client_id: "101614200308291357992",
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/project2-service-account%40project2-f5cb2.iam.gserviceaccount.com",
-  universe_domain: "googleapis.com",
-};
+export const serviceAccount: any = {};
 
 
-export const upstashUrl = "https://witty-moth-5341.upstash.io";
-export const upstashKey = "ARTdAAImcDIzYmY1Y2U4ZjdjZjg0NmRjYWJkZjljZjRlYjdmZGQxY3AyNTM0MQ";
+export const upstashUrl = "yourupstashurl";
+export const upstashKey = "yourupstashkey";
